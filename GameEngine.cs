11b2066211d917f9dfb7fae03cbfe22506ffcc06
Vlad Badica <vlad.badica@gmail.com>
2024-01-20 16:48:43 +0000

    Added game engine

---
+++ GameEngine.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using RythmGame.Utils;
+using Microsoft.Xna.Framework.Input;
+using RythmGame.GameObjects;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RythmGame
+{
+    public class GameEngine
+    {
+        private TrackBall trackBall;
+        private Map map;
+
+
+        public GameEngine()
+        {
+            trackBall = new TrackBall();
+            map = new Map();
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            trackBall.Draw(spriteBatch);
+            map.Draw(spriteBatch);
+        }
+
+        public void Initialize()
+        {
+            trackBall.Initialize();
+            map.Initialize();
+        }
+
+        public void LoadContent()
+        {
+            trackBall.LoadContent();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (InputHandler.IsKeyPressed(Keys.Space))
+            {
+
+            }
+        }
+    }
+}
--- Main.cs
+++ Main.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using RythmGame.GameObjects;
 using RythmGame.Utils;
 
 namespace RythmGame
@@ -10,8 +9,7 @@ namespace RythmGame
     {
         private GraphicsDeviceManager graphics;
         private SpriteBatch spriteBatch;
-        private TrackBall trackBall;
-        private Map map;
+        private GameEngine gameEngine;
 
         public Main()
         {
@@ -19,8 +17,7 @@ namespace RythmGame
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            map = new Map();
-            trackBall = new TrackBall();
+            gameEngine = new GameEngine();
         }
 
         protected override void Draw(GameTime gameTime)
@@ -28,9 +25,7 @@ namespace RythmGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap);
-
-            trackBall.Draw(spriteBatch);
-            map.Draw(spriteBatch);
+            gameEngine.Draw(spriteBatch);
 
             spriteBatch.End();
 
@@ -40,8 +35,7 @@ namespace RythmGame
         protected override void Initialize()
         {
             AssetManager.Initialize(Content);
-            trackBall.Initialize();
-            map.Initialize();
+            gameEngine.Initialize();
 
             graphics.PreferredBackBufferHeight = Configuration.WindowHeight;
             graphics.PreferredBackBufferWidth = Configuration.WindowWidth;
@@ -53,13 +47,13 @@ namespace RythmGame
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            trackBall.LoadContent();
+            gameEngine.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
             InputHandler.Update();
-            trackBall.Update(gameTime);
+            gameEngine.Update(gameTime);
 
             if (Keyboard.GetState().IsKeyDown(Keys.Escape)) 
             {
