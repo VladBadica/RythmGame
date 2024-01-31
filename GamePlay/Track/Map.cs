4e814a71cafdd828e6c0b0c6919a2a5fa62743fe
Vlad Badica <vlad.badica@gmail.com>
2024-01-31 19:23:09 +0000

    cleanup

--- GamePlay/Track/Map.cs
+++ GamePlay/Track/Map.cs
@@ -27,12 +27,6 @@ namespace RythmGame.GamePlay.Track
 
         public Rectangle CurrentStep => steps[0];
 
-        private void AddStep(int posX)
-        {
-            int posY = steps.Count == 0 ? Globals.WindowHeight - 32 - 50 : steps[steps.Count - 1].Y - 32;
-            steps.Add(new Rectangle(posX, posY, 3, 32));
-        }
-
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(BackgroundTexture, new Rectangle(0, 0, Globals.WindowWidth, Globals.WindowHeight), Color.White);
@@ -42,35 +36,6 @@ namespace RythmGame.GamePlay.Track
         public void Reset()
         {
             steps = new List<Rectangle>(StepsTemplate);
-            /*for (int i = 0; i < 3; i++)
-            {
-                AddStep(420);
-                AddStep(Globals.WindowWidth - 420);
-            }
-            AddStep(470);
-            AddStep(Globals.WindowWidth - 470);
-            AddStep(470);
-            for (int i = 0; i < 3; i++)
-            {
-                AddStep(Globals.WindowWidth - 420);
-                AddStep(420);
-            }
-            AddStep(Globals.WindowWidth - 490);
-            for (int i = 0; i < 5; i++)
-            {
-                AddStep(460);
-                AddStep(Globals.WindowWidth - 460);
-            }
-            for (int i = 0; i < 10; i++)
-            {
-                AddStep(440);
-                AddStep(Globals.WindowWidth - 440);
-            }
-            for (int i = 0; i < 300; i++)
-            {
-                AddStep(450);
-                AddStep(Globals.WindowWidth - 450);
-            }*/
         }
 
         public void NextStep()
